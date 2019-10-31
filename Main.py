cfcd6f53fbfcc107cf76629206a8700dde094988
Weeping-Willow <vitolinseriks@gmail.com>
2019-10-31 19:48:10 +0200

    Nice

--- Main.py
+++ Main.py
@@ -2,3 +2,11 @@ import turtle
 
 window = turtle.Screen()
 window.title("Ping pong")
+window.bgcolor("black")
+window.setup(width=800, height=600)
+window.tracer(0) #makes the game faster
+#init the game
+
+#init game loop
+while True:
+    window.update()
