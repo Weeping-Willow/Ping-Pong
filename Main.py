a80debaf103b635310421626be5ff3c602802702
Weeping-Willow <vitolinseriks@gmail.com>
2019-10-31 21:38:28 +0200

    Done game

--- Main.py
+++ Main.py
@@ -10,7 +10,7 @@ class Paddle:
         self.paddle.shapesize(stretch_wid=5,stretch_len=1) #stretches the with by x5 times and len by x1
         self.paddle.penup()  # removes a line thing in the middle
         self.paddle.goto(x,y) #pos of the thing
-
+        self.score =0
     def paddle_up(self):
         y1 = self.paddle.ycor()#gets y cord
         y1+=20
@@ -30,8 +30,8 @@ class Ball:
         self.ball.color("white")
         self.ball.penup()
         self.ball.goto(x, y)
-        self.dx = 0.2
-        self.dy = 0.2
+        self.dx = 0.15
+        self.dy = 0.15
 
 
 #init the game
@@ -54,7 +54,14 @@ window.onkeypress(p1.paddle_down, "s")
 window.onkeypress(p2.paddle_up, "Up")
 window.onkeypress(p2.paddle_down, "Down")
 
-
+#Table
+table = turtle.Turtle()
+table.speed(0)#animation speed
+table.color("white")
+table.penup()
+table.hideturtle()
+table.goto(0,260)
+table.write("Player A: {} Player B: {}".format(p1.score,p2.score), align="center", font=("Arial", 26, "normal"))
 
 #init game loop
 while True:
@@ -72,8 +79,20 @@ while True:
         b1.ball.sety(-290)
         b1.dy *= -1
     if b1.ball.xcor() > 390:
-        b1.ball.setx(390)
+        b1.ball.goto(0,0)
         b1.dx *= -1
+        p1.score +=1
+        table.clear()
+        table.write("Player A: {} Player B: {}".format(p1.score, p2.score), align="center",font=("Arial", 26, "normal"))
     if b1.ball.xcor() < -390:
-        b1.ball.setx(-390)
+        b1.ball.goto(0, 0)
         b1.dx *= -1
+        p2.score += 1
+        table.clear()
+        table.write("Player A: {} Player B: {}".format(p1.score, p2.score), align="center", font=("Arial", 26, "normal"))
+
+    #I want to punish this terrible ball AKA it touches the paddle it goes back
+    if b1.ball.xcor()<-340 and (b1.ball.ycor() < p1.paddle.ycor() + 50 and b1.ball.ycor() > p1.paddle.ycor() - 50):#
+        b1.dx *=-1
+    if b1.ball.xcor()>340 and (b1.ball.ycor() < p2.paddle.ycor() + 50 and b1.ball.ycor() > p2.paddle.ycor() - 50):#
+        b1.dx *=-1
